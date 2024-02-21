6cdced40419ca00f619e4c76e9cc522f7b466b79
Yassir <yassir.elfilali-lamdaghri@uit.ac.ma>
2024-02-21 12:00:09 +0100

    add notes

--- etudiant.java
+++ etudiant.java
@@ -1,13 +1,16 @@
 public class Etudiant {
     // Propriétés
     private String nom;
-    private String prenom
+    private String prenom;
     private int age;
+    private double note;
     
     // Constructeur
-    public Etudiant(String nom, int age) {
+    public Etudiant(String nom, String prenom, int age, double note) {
         this.nom = nom;
+        this.prenom = prenom;
         this.age = age;
+        this.note = note;
     }
     
     // Méthodes d'accès (getters) pour chaque propriété
@@ -15,8 +18,16 @@ public class Etudiant {
         return nom;
     }
     
+    public String getPrenom() {
+        return prenom;
+    }
+    
     public int getAge() {
         return age;
+    }
+    
+    public double getNote() {
+        return note;
     }
     
     // Méthodes de modification (setters) pour chaque propriété
@@ -24,7 +35,15 @@ public class Etudiant {
         this.nom = nom;
     }
     
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
     public void setAge(int age) {
         this.age = age;
+    }
+    
+    public void setNote(double note) {
+        this.note = note;
     }
 }
